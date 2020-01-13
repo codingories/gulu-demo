6d742b8795aaf0d194c204da924157b46f343a87
Ories Zhu <tellt@126.com>
2020-01-13 16:06:33 +0800

    1.13

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,17 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:'true'}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#i${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-  export default {}
+  export default {
+    props: ['icon', 'iconPosition']
+  };
 </script>
 <style lang="scss">
   .g-button {
@@ -12,6 +21,10 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     &:hover {
       border-color: var(--border-color-hover);
     }
@@ -22,6 +35,20 @@
 
     &:focus {
       outline: none;
+    }
+    > .icon {
+      order: 1;
+    }
+    > .content {
+      order: 2;
+    }
+    &.icon-right {
+      > .icon {
+        order: 2;
+      }
+      > .content {
+        order: 1;
+      }
     }
   }
 </style>
