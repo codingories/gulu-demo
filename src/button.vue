7b237b3839a65298f8160ce87e0b061e1e703491
Ories Zhu <tellt@126.com>
2020-01-13 19:24:21 +0800

    添加菊花

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,7 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
     <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -22,6 +23,15 @@
   };
 </script>
 <style lang="scss">
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
   .g-button {
     font-size: var(--font-size);
     height: var(--button-height);
@@ -61,6 +71,9 @@
         margin-right: 0;
         margin-left: .3em;
       }
+    }
+    .loading{
+      animation: spin 2s infinite linear;
     }
   }
 </style>
