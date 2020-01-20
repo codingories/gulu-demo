bb8d6ee0a097fb5c15b765e413871baeb33df121
Ories Zhu <tellt@126.com>
2020-01-20 15:13:08 +0800

    更新 README

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # 轱辘 - 一个 Vue UI 组件
 
-作者:ories
+[![Build Status](https://www.travis-ci.org/codingories/gulu-demo.svg?branch=master)](https://www.travis-ci.org/codingories/gulu-demo)
+
+## 介绍
+
+## 开始使用
 
 使用本框架前，请在CSS中开启border-box
 
 ```
 * {box-sizing: border-box;}
 ```
+
+## 文档
+
+## 提问
+
+## 变更记录
+
+## 联系方式
+
+## 贡献代码
+
+
--- src/button.vue
+++ src/button.vue
@@ -1,5 +1,9 @@
 <template>
-  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+  <button
+    class="g-button"
+    :class="{[`icon-${iconPosition}`]:true}"
+    @click="$emit('click')"
+  >
     <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
     <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
     <div class="content">
@@ -8,82 +12,82 @@
   </button>
 </template>
 <script>
-  // import Vue from 'vue'
-  import Icon from './icon'
-  // Vue.component('g-icon',Icon) // 全局注册的方法不好，该用局部注册
-  export default {
-    // props: ['icon', 'iconPosition'],
-    components:{
-      'g-icon':Icon
+// import Vue from 'vue'
+import Icon from "./icon";
+// Vue.component('g-icon',Icon) // 全局注册的方法不好，该用局部注册
+export default {
+  // props: ['icon', 'iconPosition'],
+  components: {
+    "g-icon": Icon
+  },
+  props: {
+    icon: {},
+    loading: {
+      type: Boolean,
+      default: false
     },
-    props: {
-      icon: {},
-      loading: {
-        type: Boolean,
-        default: false
-      },
-      iconPosition: {
-        type: String,
-        default: 'left',
-        validator: function (value) {
-          return value === 'left' || value === 'right';
-        }
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator: function(value) {
+        return value === "left" || value === "right";
       }
     }
-  };
+  }
+};
 </script>
 <style lang="scss">
-  @keyframes spin {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em; /*width*/
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {
+    border-color: var(--border-color-hover);
   }
 
-  .g-button {
-    font-size: var(--font-size);
-    height: var(--button-height);
-    padding: 0 1em; /*width*/
-    border-radius: var(--border-radius);
-    border: 1px solid var(--border-color);
-    background: var(--button-bg);
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    vertical-align: middle;
-    &:hover {
-      border-color: var(--border-color-hover);
-    }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
 
-    &:active {
-      background-color: var(--button-active-bg);
-    }
+  &:focus {
+    outline: none;
+  }
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.3em;
+  }
 
-    &:focus {
-      outline: none;
-    }
+  &.icon-right {
     > .content {
-      order: 2;
+      order: 1;
     }
     > .icon {
-      order: 1;
-      margin-right: .3em;
-    }
-
-    &.icon-right {
-      > .content {
-        order: 1;
-      }
-      > .icon {
-        order: 2;
-        margin-right: 0;
-        margin-left: .3em;
-      }
-    }
-    .loading {
-      animation: spin 2s infinite linear;
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.3em;
     }
   }
+  .loading {
+    animation: spin 2s infinite linear;
+  }
+}
 </style>
