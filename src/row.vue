e372ff00fe1d1b678c2da16e476f24a842939885
Ories Zhu <tellt@126.com>
2020-01-22 18:30:25 +0800

    第四个视频完成

--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,27 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
+<script>
+  export default {
+    name: 'GuluRow',
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    created(){
+      console.log(this.$children);
+    },
+    mounted(){
+      console.log(this.$children);
+      this.$children.forEach((vm)=>{
+        vm.gutter = this.gutter
+      })
+    }
+  }
+</script>
 <style scoped lang="scss">
   .row {
     display: flex;
