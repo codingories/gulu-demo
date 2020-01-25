2f0dfb0ceb1a0474228397d774309d1a7c537e3a
Ories Zhu <tellt@126.com>
2020-01-25 16:40:30 +0800

    添加简单动画

--- src/layout.vue
+++ src/layout.vue
@@ -29,7 +29,6 @@
     flex-grow: 1;
     display: flex;
     flex-direction: column;
-    border: 1px solid red;
     &.hasSider{
       flex-direction: row;
     }
--- src/sider.vue
+++ src/sider.vue
@@ -1,17 +1,41 @@
 <template>
-  <div>
-    <slot class="sider"></slot>
-  </div>
+  <transition name="slide">
+    <div class="sider" v-if="visible">
+      <slot></slot>
+      <button @click="visible=false">close</button>
+    </div>
+  </transition>
+
 </template>
 
 <script lang="ts">
   export default {
-    name: "GuluSider"
+    name: "GuluSider",
+    data(){
+      return {
+        visible: true
+      }
+    },
+    methods:{
+
+    }
+
   }
 </script>
 
 <style lang="scss" scoped>
   .sider {
-
+    position: relative;
+    > button {
+      position: absolute;
+      top:0;
+      right:0;
+    }
+  }
+  .slide-enter-active, .slide-leave-active {
+    transition: all .5s;
+  }
+  .slide-enter, .slide-leave-to{
+    margin-left: -200px;
   }
 </style>
