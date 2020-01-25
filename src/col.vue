ed9d383185b4f04d5fa6cbfa5b1ac5215b686fed
Ories Zhu <tellt@126.com>
2020-01-25 11:05:53 +0800

    响应式基本完成

--- src/col.vue
+++ src/col.vue
@@ -39,10 +39,10 @@
         return [
           span && `col-${span}`,
           offset && `offset-${offset}`,
-          ...(ipad ? [`col-ipad-${ipad.span}`]:[]),
-          ...(narrowPc ? [`col-narrow-pc-${narrowPc.span}`]:[]),
-          ...(pc ? [`col-pc-${pc.span}`]:[]),
-          ...(widePc ? [`col-wide-pc-${widePc.span}`]:[])
+          ...(ipad ? [`col-ipad-${ipad.span}`] : []),
+          ...(narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
+          ...(pc ? [`col-pc-${pc.span}`] : []),
+          ...(widePc ? [`col-wide-pc-${widePc.span}`] : [])
         ]
       },
       colStyle() {
@@ -56,9 +56,6 @@
 </script>
 <style scoped lang="scss">
   .col {
-    height: 30px;
-    /*width: 50%;*/
-
     $class-prefix: col-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
@@ -71,7 +68,7 @@
         margin-left: ($n / 24) * 100%
       }
     }
-    @media (min-width: 577px) and (max-width: 768px) {
+    @media (min-width: 577px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -85,7 +82,7 @@
         }
       }
     }
-    @media (min-width: 769px) and (max-width: 992px) {
+    @media (min-width: 769px) {
       $class-prefix: col-narrow-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -99,7 +96,7 @@
         }
       }
     }
-    @media (min-width: 993px) and (max-width: 1200px) {
+    @media (min-width: 993px) {
       $class-prefix: col-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
