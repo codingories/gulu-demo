14f2981346e3283d1bd9f1338b18e8636555980d
Ories Zhu <tellt@126.com>
2020-01-26 21:40:59 +0800

    优化三种动画

--- src/app.js
+++ src/app.js
@@ -38,9 +38,18 @@ new Vue({
   },
 
   methods: {
-    showToast() {
+    showToast1(){
+      this.showToast('top')
+    },
+    showToast2(){
+      this.showToast('middle')
+    },
+    showToast3(){
+      this.showToast('bottom')
+    },
+    showToast(position) {
       this.$toast('你的智商需要充值', {
-        position: 'bottom',
+        position,
         enableHtml: false,
         closeButton: {
           text: '已充值',
--- src/toast.vue
+++ src/toast.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="toast" ref="wrapper" :class="toastClasses">
-    <div class="message">
-      <slot v-if="!enableHtml"></slot>
-      <div v-else v-html="$slots.default[0]"></div>
-    </div>
-    <div class="line" ref="line"></div>
-    <span v-if="closeButton" class="close" @click="onClickClose">
+  <div class="wrapper" :class="toastClasses">
+    <div class="toast" ref="toast">
+      <div class="message">
+        <slot v-if="!enableHtml"></slot>
+        <div v-else v-html="$slots.default[0]"></div>
+      </div>
+      <div class="line" ref="line"></div>
+      <span v-if="closeButton" class="close" @click="onClickClose">
       {{closeButton.text}}
     </span>
+    </div>
   </div>
 </template>
 
@@ -23,9 +25,9 @@
         type: Number,
         default: 5
       },
-      closeButton:{
+      closeButton: {
         type: Object,
-        default(){
+        default() {
           return {
             text: '关闭', callback: undefined
           }
@@ -38,20 +40,20 @@
       position: {
         type: String,
         default: 'top',
-        validator(value){
+        validator(value) {
           return ['top', 'bottom', 'middle'].indexOf(value) >= 0
         }
       }
     },
-    created(){
+    created() {
       // console.log(11111)
       // console.log(this.closeButton.callback)
       // this.closeButton.callback()
     },
-    computed:{
-      toastClasses(){
+    computed: {
+      toastClasses() {
         return {
-          [`position-${this.position}`]:true
+          [`position-${this.position}`]: true
         }
       }
     },
@@ -60,16 +62,16 @@
       this.execAutoClose();
     },
     methods: {
-      updateStyles(){
+      updateStyles() {
         if (this.autoClose) {
           setTimeout(() => {
             this.close()
           }, this.autoCloseDelay * 100000)
         }
       },
-      execAutoClose(){
-        this.$nextTick(()=>{
-          this.$refs.line.style.height = `${this.$refs.wrapper.getBoundingClientRect().height}px`
+      execAutoClose() {
+        this.$nextTick(() => {
+          this.$refs.line.style.height = `${this.$refs.toast.getBoundingClientRect().height}px`
         })
       },
       close() {
@@ -77,12 +79,12 @@
         this.$emit('close')
         this.$destroy()
       },
-      log(){
+      log() {
         console.log('测试')
       },
-      onClickClose(){
+      onClickClose() {
         this.close()
-        if(this.closeButton && typeof this.closeButton.callback === 'function'){
+        if (this.closeButton && typeof this.closeButton.callback === 'function') {
           this.closeButton.callback(this) // this就是toast实例
         }
       },
@@ -94,16 +96,73 @@
   $font-size: 14px;
   $toast-min-height: 40px;
   $toast-bg: rgba(0, 0, 0, 0.75);
-  @keyframes fade {
-    0% {opacity: 0; transform: translateY(100%);}
-    100% {opacity: 1;transform: translateY(0);}
+  $animation-duration: 300ms;
+  @keyframes slide-up {
+    0% {
+      opacity: 0;
+      transform: translateY(100%);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
   }
-  .toast {
-    animation: fade 1s;
-    font-size: $font-size;
-    min-height:$toast-min-height;
-    line-height: 1.8;
+
+  @keyframes fade-in {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-down {
+    0% {
+      opacity: 0;
+      transform: translateY(-100%);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+
+  .wrapper {
     position: fixed;
+    left: 50%;
+    transform: translate(-50%);
+    &.position-top {
+      top: 0;
+      .toast {
+        border-top-left-radius:0;
+        border-top-right-radius: 0;
+        animation: slide-down $animation-duration;
+      }
+    }
+
+    &.position-bottom {
+      bottom: 0;
+      .toast {
+        border-bottom-left-radius:0;
+        border-bottom-right-radius: 0;
+        animation: slide-up $animation-duration;
+      }
+    }
+
+    &.position-middle {
+      top: 50%;
+      transform: translateX(-50%) translateY(-50%);
+      .toast {
+        animation: fade-in $animation-duration;
+      }
+    }
+  }
+
+  .toast {
+    font-size: $font-size;
+    min-height: $toast-min-height;
+    line-height: 1.8;
     display: flex;
     align-items: center;
     color: white;
@@ -111,30 +170,20 @@
     border-radius: 4px;
     box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
     padding: 0 16px;
-    left: 50%;
+
     .message {
       padding: 8px 0;
     }
-    .close{
+
+    .close {
       padding-left: 16px;
       flex-shrink: 0;
     }
-    .line{
+
+    .line {
       height: 100%;
       border-left: 1px solid #666;
       margin-left: 16px;
-    }
-    &.position-top{
-      top: 0;
-      transform: translate(-50%);
-    }
-    &.position-bottom{
-      bottom: 0;
-      transform: translate(-50%);
-    }
-    &.position-middle{
-      top: 50%;
-      transform: translate(-50%, -50%);
     }
   }
 
