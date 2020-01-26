fbff4a913f027f602444603c71f8524d7198fa69
Ories Zhu <tellt@126.com>
2020-01-26 20:54:52 +0800

    实现进入动画

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ new Vue({
   methods: {
     showToast() {
       this.$toast('你的智商需要充值', {
-        position: 'top',
+        position: 'bottom',
         enableHtml: false,
         closeButton: {
           text: '已充值',
--- src/toast.vue
+++ src/toast.vue
@@ -94,7 +94,12 @@
   $font-size: 14px;
   $toast-min-height: 40px;
   $toast-bg: rgba(0, 0, 0, 0.75);
+  @keyframes fade {
+    0% {opacity: 0; transform: translateY(100%);}
+    100% {opacity: 1;transform: translateY(0);}
+  }
   .toast {
+    animation: fade 1s;
     font-size: $font-size;
     min-height:$toast-min-height;
     line-height: 1.8;
