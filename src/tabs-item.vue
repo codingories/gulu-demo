15178c5ded7c9c5c01070bb7224e87f6e3f83044
Ories Zhu <tellt@126.com>
2020-01-28 19:52:44 +0800

    增加disabled功能

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="xxx" :class="classes" >
+  <div class="tabs-item" @click="onClick" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -26,7 +26,8 @@
     computed: {
       classes() { // classes是一个计算属性
         return {
-          active: this.active
+          active: this.active,
+          disabled: this.disabled,
         }
       }
     },
@@ -36,7 +37,10 @@
       })
     },
     methods: {
-      xxx() {
+      onClick() {
+        if (this.disabled) {
+          return
+        }
         this.eventBus.$emit('update:selected', this.name, this)
       }
     }
@@ -45,16 +49,21 @@
 
 <style lang="scss" scoped>
   $blue: blue;
+  $disabled-text-color: grey;
   .tabs-item {
     flex-shrink: 0;
     padding: 0 2em;
-    height:100%;
+    height: 100%;
     align-items: center;
     display: flex;
     cursor: pointer;
-    &.active{
+    &.active {
       color: $blue;
       font-weight: bold;
+    }
+    &.disabled {
+      color: $disabled-text-color;
+      cursor: not-allowed;
     }
   }
 </style>
