4d4fcb4cb504b9a62cf7cd465b72a6b97de3bed6
Ories Zhu <tellt@126.com>
2020-01-28 21:58:20 +0800

    添加tabs部分测试用例

---
+++ test/tabs.test.js
@@ -0,0 +1,54 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Tabs from '../src/tabs'
+import TabsHead from '../src/tabs-head'
+import TabsBody from '../src/tabs-body'
+import TabsItem from '../src/tabs-item'
+import TabsPane from '../src/tabs-pane'
+
+Vue.component('g-tabs', Tabs)
+Vue.component('g-tabs-head', TabsHead)
+Vue.component('g-tabs-body', TabsBody)
+Vue.component('g-tabs-item', TabsItem)
+Vue.component('g-tabs-pane', TabsPane)
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Tabs', () => {
+  it('存在.', () => {
+    expect(Tabs).to.exist
+  })
+
+  it('接收 selected 属性', (done) => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+      <g-tabs selected="finance" >
+        <g-tabs-head>
+          <g-tabs-item name="woman"> 美女 </g-tabs-item>
+          <g-tabs-item name="finance" > 财经 </g-tabs-item>
+          <g-tabs-item name="sports"> 体育 </g-tabs-item>
+        </g-tabs-head>
+        <g-tabs-body> 
+          <g-tabs-pane name="woman"> 美女相关咨询 </g-tabs-pane> 
+          <g-tabs-pane name="finance"> 财经相关咨询 </g-tabs-pane> 
+          <g-tabs-pane name="sports"> 体育相关咨询 </g-tabs-pane>
+        </g-tabs-body>
+      </g-tabs>
+    `
+    const vm = new Vue({
+      el: div
+    })
+
+    vm.$nextTick(()=>{
+      let x = vm.$el.querySelector(`.tabs-item[data-name="finance"]`)
+      expect(x.classList.contains('active')).to.be.true
+      done()
+    })
+  })
+
+  it('可以接收 direction prop', ()=>{
+
+  })
+});
