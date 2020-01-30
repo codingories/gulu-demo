3ee3086a449772bfdbeab1e6c1898cfee8de2af9
Ories Zhu <tellt@126.com>
2020-01-30 12:22:20 +0800

    基本完成popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="popover" @click.stop="xxx">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click="onClick" ref="popover">
+    <!--    .stop-->
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
+      <!--      @click.stop-->
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
@@ -12,48 +14,77 @@
 <script lang="ts">
   export default {
     name: "GuluPopover",
-    data (){
+    data() {
       return {
-        visible:false
+        visible: false
       }
     },
     methods: {
-      xxx(){
-        this.visible = !this.visible
-        if(this.visible === true) {
-          setTimeout(()=>{
-            document.body.appendChild(this['$refs']['contentWrapper'])
-            let {top, left} =this['$refs']['triggerWrapper'].getBoundingClientRect()
-            this['$refs']['contentWrapper'].style.left = left + window.scrollX + 'px'
-            this['$refs']['contentWrapper'].style.top = top + window.scrollY +'px'
-            let eventHandler = ()=>{
-              this.visible = false;
-              console.log('document 隐藏 popover')
-              document.removeEventListener('click',eventHandler)
-            }
-            document.addEventListener('click', eventHandler)
-          })
-        }else{
-          console.log('vm 隐藏 popover')
+      positionContent() {
+        document.body.appendChild(this["$refs"]["contentWrapper"])
+        let {top, left} = this["$refs"]["triggerWrapper"].getBoundingClientRect()
+        this["$refs"]["contentWrapper"].style.left = left + window.scrollX + "px"
+        this["$refs"]["contentWrapper"].style.top = top + window.scrollY + "px"
+      },
+      listenToDocument() {
+        let eventHandler = (e) => {
+          if (this["$refs"]["contentWrapper"].contains(e.target)) { // 这句话解决了点击内容消失的问题
+          } else {
+            this.visible = false
+            console.log('关闭')
+            document.removeEventListener("click", eventHandler)
+            console.log("关闭") // document事件引起的
+          }
+        }
+        document.addEventListener("click", eventHandler)
+      },
+      listenToDocument(){
+        let eventHandler = (e) => {
+          if ( this["$refs"]["contentWrapper"] &&
+            this["$refs"]["contentWrapper"].contains(e.target)) { // 这句话解决了点击内容消失的问题
+            return
+          } else {
+            this.visible = false
+            document.removeEventListener("click", eventHandler)
+            console.log("关闭") // document事件引起的
+          }
+        }
+        document.addEventListener("click", eventHandler)
+      },
+      opShow(){
+        setTimeout(() => {
+          this.positionContent()
+          this.listenToDocument()
+        })
+      },
+      onClick(event) {
+        if (this["$refs"]["triggerWrapper"].contains(event.target)) {
+          this.visible = !this.visible
+          if (this.visible === true) {
+            this.opShow()
+          } else {
+            console.log('关闭')
+          }
         }
       }
     },
-    mounted(){
+    mounted() {
       console.log(this.$refs.triggerWrapper)
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  .popover{
+  .popover {
     display: inline-block;
     vertical-align: top;
     position: relative;
   }
-  .content-wrapper{ // 如果写了scoped,popover里面那么就只作用于popover里面,移到外面就在外面了就可以
+
+  .content-wrapper { // 如果写了scoped,popover里面那么就只作用于popover里面,移到外面就在外面了就可以
     position: absolute;
     border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
     transform: translateY(-100%);
   }
 </style>
