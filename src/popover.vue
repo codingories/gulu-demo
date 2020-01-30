630a3e3279541be78127e6ae73ece586f06eb9af
Ories Zhu <tellt@126.com>
2020-01-30 11:18:41 +0800

    解决当前容器有 overflow hidden 造成的 bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="popover" @click.stop="xxx">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -20,6 +22,10 @@
         this.visible = !this.visible
         if(this.visible === true) {
           setTimeout(()=>{
+            document.body.appendChild(this['$refs']['contentWrapper'])
+            let {top, left} =this['$refs']['triggerWrapper'].getBoundingClientRect()
+            this['$refs']['contentWrapper'].style.left = left + window.scrollX + 'px'
+            this['$refs']['contentWrapper'].style.top = top + window.scrollY +'px'
             let eventHandler = ()=>{
               this.visible = false;
               console.log('document 隐藏 popover')
@@ -31,6 +37,9 @@
           console.log('vm 隐藏 popover')
         }
       }
+    },
+    mounted(){
+      console.log(this.$refs.triggerWrapper)
     }
   }
 </script>
@@ -40,12 +49,11 @@
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper{
-      position: absolute;
-      bottom: 100%;
-      left: 0;
-      border: 1px solid red;
-      box-shadow: 0 0 3px rgba(0,0,0,0.5);
-    }
+  }
+  .content-wrapper{ // 如果写了scoped,popover里面那么就只作用于popover里面,移到外面就在外面了就可以
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    transform: translateY(-100%);
   }
 </style>
