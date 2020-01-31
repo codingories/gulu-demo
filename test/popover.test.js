e0bcc0a257a8d3f2dafe1fce43571b3faac1dcaa
Ories Zhu <tellt@126.com>
2020-01-31 16:06:28 +0800

    popover的第一个测试用例-position

---
+++ test/popover.test.js
@@ -0,0 +1,36 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Popover from '../src/popover'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Popover', () => {
+
+  it('存在.', () => {
+    expect(Popover).to.be.ok // 非falsy就是ok的
+  })
+  it('可以设置position.', (done) => {
+    Vue.component('g-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+    <g-popover position="bottom" ref="a">
+      <template slot="content">
+        弹出内容
+      </template>
+      <button>点我</button>
+    </g-popover>
+    `
+    const vm = new Vue({ // 这个vm不是组件，这个vm是一个div对应的实例,div里面才是popover组件
+      el: div
+    })
+    vm.$el.querySelector('button').click()
+    vm.$nextTick(() => {
+      const {contentWrapper} = vm.$refs.a.$refs
+      expect(contentWrapper.classList.contains('position-top')).to.be.false
+      done()
+    })
+  })
+
+});
