c142f76812a84d30e75f54d4dbfeb7688e2f69cc
Ories Zhu <tellt@126.com>
2020-02-01 10:54:14 +0800

    可以设置默认selected

---
+++ src/collapse-item.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="collapseItem">
+    <div class="title" @click="toggle">
+      {{title}}
+    </div>
+    <div class="content" v-if="open">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: "GuluCollapseItem",
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        open: false
+      }
+    },
+    inject: ['eventBus'],
+
+    mounted(){
+      this.eventBus && this.eventBus.$on('update:selected', (name)=>{
+        if (name !== this.name){
+          this.close()
+        }else{
+          this.show()
+        }
+      })
+    },
+    methods:{
+      toggle(){
+        if(this.open) {
+          this.open = false
+        }else{
+          this.eventBus && this.eventBus.$emit('update:selected', this.name)
+        }
+      },
+      close(){
+        this.open=false;
+      },
+      show(){
+        this.open = true
+      }
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  $grey: #ddd;
+  $border-radius: 4px;
+  .collapseItem {
+    > .title {
+      border: 1px solid $grey;
+      margin-top: -1px;
+      margin-left: -1px;
+      margin-right: -1px;
+      min-height: 32px;
+      display: flex;
+      align-items: center;
+      padding: 0 8px;
+    }
+
+    &:first-child {
+      > .title {
+        border-top-left-radius: $border-radius;
+        border-top-right-radius: $border-radius;
+      }
+    }
+
+    &:last-child {
+      > .title:last-child {
+        margin-bottom: -1px;
+        border-bottom-left-radius: $border-radius;
+        border-bottom-right-radius: $border-radius;
+      }
+    }
+
+
+    > .content {
+      padding: 8px;
+    }
+  }
+</style>
---
+++ src/collapse.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from "vue"
+
+  export default {
+    name: "GuluCollapse",
+    props: {
+      single: {
+        type: Boolean,
+        default: false
+      },
+      selected: {
+        type: String,
+      }
+    },
+    data() {
+      return {
+        eventBus: new Vue()
+      }
+    },
+    provide() {
+        return {
+          eventBus: this.eventBus
+        }
+    },
+    mounted(){
+      this.eventBus.$emit('update:selected',this.selected)
+    }
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  $grey: #ddd;
+  $border-radius: 4px;
+  .collapse {
+    border: 1px solid $grey;
+    border-radius: $border-radius;
+  }
+</style>
